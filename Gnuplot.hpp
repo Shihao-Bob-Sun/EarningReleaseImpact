c5b3fccf10c82b8205008060099de35ab0a00420
Shihao (Bob) Sun <51458492+Shihao-Bob-Sun@users.noreply.github.com>
2019-07-07 13:27:21 -0400

    Create Gnuplot.hpp

---
+++ Gnuplot.hpp
@@ -0,0 +1,15 @@
+
+#ifndef Gnuplot_hpp
+#define Gnuplot_hpp
+
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// Plot the result using Gnuplot
+void PlotResults (vector<double> &CAAR_Beat, vector<double> &CAAR_Meet, vector<double> &CAAR_Miss);
+
+#endif /* Gnuplot_hpp */
