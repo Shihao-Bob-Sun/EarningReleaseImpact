b5e9b56a218089ce9e8dc66db86420a8dae84ccd
Shihao (Bob) Sun <51458492+Shihao-Bob-Sun@users.noreply.github.com>
2019-07-07 12:54:38 -0400

    Create Data_Preparation.hpp

---
+++ Data_Preparation.hpp
@@ -0,0 +1,25 @@
+
+#ifndef Data_Preparation_hpp
+#define Data_Preparation_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <map>
+#include "Stock.hpp"
+
+using namespace std;
+
+// Load trading calendar from .csv file and populate the date-return map for SPY
+int GetSPYDataAndTradingCal(ifstream *File, vector<string>& tradingCalendar, map<string, double>& SPYData);
+
+// Populate the map for Beat, Meet, and Miss group
+int PopulateMap (ifstream *File, vector<string> &tradingCalendar, map<string,Stock>& Beat, map<string,Stock>& Meet, map<string,Stock>& Miss);
+
+// Clean the map by removing the stock with less than 121 days price data
+void CleanMap (map<string, Stock> &Beat, map<string, Stock> &Meet, map<string, Stock> &Miss,
+               vector<string> &BeatStock, vector<string> &MeetStock, vector<string> &MissStock, vector<string> &symbolList);
+
+#endif /* Data_Preparation_hpp */
